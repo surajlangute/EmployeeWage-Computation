6e94b6de54562f8944838df077bbdb830b89d7ca
surajlangute <surajlangute55@gmail.com>
2022-09-07 13:55:23 +0530

    Added : Daily Employee Wage

--- src/com/employee/wage/employeeWage.java
+++ src/com/employee/wage/employeeWage.java
@@ -7,13 +7,21 @@ public class employeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation .");
 
+		int wagePerHour = 20;
+		int fullDayHours = 8;
+		int salary = 0;
+
 		Random random = new Random();
 		int randomNum = random.nextInt(2);
 
-		if (randomNum == 1)
+		if (randomNum == 1) {
 			System.out.println("Employee is Present");
+			salary = wagePerHour * fullDayHours;
+		} 
 		else
+
 			System.out.println("Employee is Absent");
+		System.out.println("Employee salary is : " + salary);
 
 	}
 
