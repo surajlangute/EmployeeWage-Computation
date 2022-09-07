18e527c92ffd21cdda8400e7c74e46aecda5c790
surajlangute <surajlangute55@gmail.com>
2022-09-07 14:47:27 +0530

    Added : Employees Monthly Wages

--- src/com/employee/wage/employeeWage.java
+++ src/com/employee/wage/employeeWage.java
@@ -11,28 +11,35 @@ public class employeeWage {
 		int fullDayHours = 8;
 		int halfDayHours = 4;
 		int salary = 0;
+		int monthlySalary = 0;
+		int workingDaysPerMonth = 20;
 
 		Random random = new Random();
 		int randomNum = random.nextInt(3);
 
-		switch (randomNum) 
-		{
-		case 0:
-			System.out.println("Employee is Absent");
-			break;
-			
-		case 1:
-			System.out.println("Employee is Present");
-			salary = wagePerHour * fullDayHours;
-			break;
+		for (int i = 1; i <= workingDaysPerMonth; i++) {
 
-		case 2:
-			System.out.println("Employee is Present for Halfday");
-			salary = wagePerHour * halfDayHours;
-			break;
+			switch (randomNum) {
+			case 0:
+				System.out.println("Employee is Absent");
+				break;
+
+			case 1:
+				System.out.println("Employee is Present");
+				salary = wagePerHour * fullDayHours;
+				break;
+
+			case 2:
+				System.out.println("Employee is Present for Halfday");
+				salary = wagePerHour * halfDayHours;
+				break;
+			}
+			monthlySalary = monthlySalary + salary;
+			System.out.println("Employees salary for day" + i + "  is : " + salary);
+
 		}
-
-		System.out.println("Employee salary is : " + salary);
+		
+		System.out.print("Employees monthly salary is : " + monthlySalary);
 
 	}
 
